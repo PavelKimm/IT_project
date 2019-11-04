26225298f5ebeb7e41965cb04a1660b4c09e6f81
Pavel Kim <p.pavelkim@gmail.com>
2019-11-04 19:44:35 +0700

    Поправлен путь к домашней странице

--- korean_portal/news/views.py
+++ korean_portal/news/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h1>News Home</h1>')
+    return HttpResponse('<h1>News Homepage</h1>')
+
+def about(request):
+    return HttpResponse('<h1>News about page</h1>')
+    
