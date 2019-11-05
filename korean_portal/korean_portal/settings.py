1bda9d35ad833579e674a841b56f7f59ac43ae04
Pavel Kim <p.pavelkim@gmail.com>
2019-11-05 20:22:00 +0700

    Revert "были загружены статические файлы, набросан шаблон для страничек"
    This reverts commit 1c66e053c02fbb19dfe11aea909517e03bbb25b5.

--- korean_portal/korean_portal/settings.py
+++ korean_portal/korean_portal/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'news.apps.NewsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
